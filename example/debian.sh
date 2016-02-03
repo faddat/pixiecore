40bf1be6613f62260297174d67900d50c9b00912
Jacob Gadikian <faddat@Jacobs-iMac.local>
2016-02-03 23:47:55 +0700

    upplied sudo

--- example/debian.sh
+++ example/debian.sh
@@ -4,4 +4,4 @@ rm debian/linux
 rm debian/initrd.gz
 wget http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
 wget http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
-pixiecore -initrd debian/initrd.gz -kernel debian/linux -cmdline="auto=true url=https://www.debian.org/releases/jessie/example-preseed.txt"
+sudo pixiecore -initrd debian/initrd.gz -kernel debian/linux -cmdline="auto=true url=https://www.debian.org/releases/jessie/example-preseed.txt"
