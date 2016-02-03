36206812f2f618697a32182bb619081e7abae0a3
Jacob Gadikian <faddat@Jacobs-iMac.local>
2016-02-04 00:12:11 +0700

    updated debian preseed file.  Make sure you fork to your own gist!h

--- example/debian.sh
+++ example/debian.sh
@@ -5,4 +5,4 @@ rm debian/linux
 rm debian/initrd.gz
 wget -O debian/linux http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
 wget -O debian/initrd.gz http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
-sudo pixiecore -initrd debian/initrd.gz -kernel debian/linux -cmdline="auto=true url=https://www.debian.org/releases/jessie/example-preseed.txt"
+sudo pixiecore -initrd debian/initrd.gz -kernel debian/linux -cmdline="auto=true url=http://gist.githubusercontent.com/faddat/6c04d3058aa1c7b524e2/raw/b342f379fcfc0827a515cdd08a6f8f042bb0c9b5/example-preseed.txt"
