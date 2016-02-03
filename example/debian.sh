6794e92c9b7bf622faa5ad3ab1124e5a140fb8e7
Jacob Gadikian <faddat@Jacobs-iMac.local>
2016-02-03 23:51:53 +0700

    added mkdir debian

--- example/debian.sh
+++ example/debian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This script will delete any existing initrd/kernel for debian in /examples/debian and then download today's latest & greatest initrd & debian.  Lastly, it will use a minimal debian preseed to autoinstall on as many hosts as you feel like rebooting.
+mkdir debian
 rm debian/linux
 rm debian/initrd.gz
 wget -O debian/linux http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
