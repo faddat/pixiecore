b8e1f57e8266dbeaacec1b2acee81a3c782e1a8b
Jacob Gadikian <faddat@Jacobs-iMac.local>
2016-02-03 23:50:33 +0700

    changed .gitignore and updated debian.sh

--- example/debian.sh
+++ example/debian.sh
@@ -2,6 +2,6 @@
 #This script will delete any existing initrd/kernel for debian in /examples/debian and then download today's latest & greatest initrd & debian.  Lastly, it will use a minimal debian preseed to autoinstall on as many hosts as you feel like rebooting.
 rm debian/linux
 rm debian/initrd.gz
-wget http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
-wget http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
+wget -O debian/linux http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/linux
+wget -O debian/initrd.gz http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/current/images/netboot/debian-installer/amd64/initrd.gz
 sudo pixiecore -initrd debian/initrd.gz -kernel debian/linux -cmdline="auto=true url=https://www.debian.org/releases/jessie/example-preseed.txt"
